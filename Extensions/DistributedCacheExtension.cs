a8cc003375a52599a98ce34a74fe1ed0ca9da61e
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2023-09-01 12:09:10 +0200

    Wraps parameters idented

--- Extensions/DistributedCacheExtension.cs
+++ Extensions/DistributedCacheExtension.cs
@@ -1,12 +1,14 @@
 ﻿using Newtonsoft.Json;
-using static rate_limit.RateLimit.RateLimitMiddleware;
+using rate_limit.RateLimit;
 
 namespace Microsoft.Extensions.Caching.Distributed;
 
 public static class DistributedCacheExtension
 {
     public async static Task<ConsumptionData?> GetCustomerConsumptionDataFromContextAsync(
-        this IDistributedCache cache, HttpContext context, CancellationToken cancellation = default)
+        this IDistributedCache cache,
+        HttpContext context,
+        CancellationToken cancellation = default)
     {
         var result = await cache.GetStringAsync(context.GetCustomerKey(), cancellation);
         if (result is null)
@@ -16,7 +18,10 @@ public static class DistributedCacheExtension
     }
 
     public async static Task SetCacheValueAsync(
-        this IDistributedCache cache, string key, ConsumptionData? customerRequests, CancellationToken cancellation = default)
+        this IDistributedCache cache,
+        string key,
+        ConsumptionData? customerRequests,
+        CancellationToken cancellation = default)
     {
         // If the customer does not yet have consumption data, we will create his record, starting the count at one request
         customerRequests ??= new ConsumptionData(DateTime.UtcNow, 1);
